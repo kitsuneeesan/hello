d1ef82c141ef0d28f4586039a5284b78fa9282e1
kitsuneeesan <rananda198@gmail.com>
2021-09-01 16:42:54 +0700

    add progress bar

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from datetime import date, datetime, timedelta
 
 class hello():
     current_datetime = datetime.now()
+
     def _get_weather(self):
         f = open(f'/home/{os.getlogin()}/.cache/weather.json', "r")
         data = json.loads(f.read())
@@ -32,6 +33,11 @@ class hello():
             elif date.strftime("%A") == 'Sunday':
                 return False
 
+    def progress(self, percent=0, widht=25):
+        left = widht * percent // 100
+        right = widht - left
+        return '▓' * left + '░' * right + f' {percent:.0f}%'
+
     def _get_year_percentage(self):
         epoch_year = date.today().year
         year_start = date(epoch_year, 1, 1)
@@ -39,24 +45,22 @@ class hello():
 
         current_date = date.today() - year_start
         total_days = year_end - year_start
-
-        persen = round(current_date.days / total_days.days * 100)
-        return f'| year progress : {persen}%'
+        return round(current_date.days / total_days.days * 100)
 
     def __init__(self):
-        year_percent = self._get_year_percentage()
+        bar = self.progress(self._get_year_percentage())
         f = open(f'/home/{os.getlogin()}/.cache/holidays.json', "r")
         holidays = json.loads(f.read())
         if self._is_day_after_tomorrow_off(holidays):
-            print(f'Lusa Libur!! {year_percent}')
+            print(f'{bar} | Lusa Libur!!')
 
         is_off = self._is_tomorrow_off(holidays)
         if type(is_off) == str:
             print('Besok Libur! |', is_off, year_percent)
         elif type(is_off) ==  bool:
-            print(f'Besok Libur! Happy Weekend! | {year_percent}')
+            print(f'{bar} | Besok Libur! Happy Weekend!')
         else:
-            print(f'あの子のために {year_percent}')
+            print(f'{bar} | あの子のために')
 
         self._get_weather()
 
